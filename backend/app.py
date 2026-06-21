cfaa78c7539c5531d0d9d9f2ecef514130759b02
Jiya Pardeshi <nayanav934@gmail.com>
2026-06-21 09:08:44 +0530

    Changed the port settings

--- backend/app.py
+++ backend/app.py
@@ -117,5 +117,8 @@ def parcel_test():
     })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", 8080))
+)
 
